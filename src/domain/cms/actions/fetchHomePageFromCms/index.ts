42431b16410f28897a651848c31e136d8bd54e74
Nejc <nejc@vabisabi.com>
2025-10-18 15:25:48 +0200

    Removes dynamic validation

--- src/domain/cms/actions/fetchHomePageFromCms/index.ts
+++ src/domain/cms/actions/fetchHomePageFromCms/index.ts
@@ -1,11 +1,7 @@
 import { prismicClient } from "@/services/prismic";
 
 export const fetchHomePageFromCms = async () => {
-  const homePage = await prismicClient
-    .getSingle("home_page", {
-      fetchOptions: { next: { revalidate: 0 } },
-    })
-    .catch(() => null);
+  const homePage = await prismicClient.getSingle("home_page").catch(() => null);
 
   if (!homePage) {
     return null;
