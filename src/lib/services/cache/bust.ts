3b7fcb65f6db97966533366330eec235636a0d78
Nejc <nejc@vabisabi.com>
2026-04-10 19:17:39 +0200

    Reverting tickets from scanned status in admin

--- src/lib/services/cache/bust.ts
+++ src/lib/services/cache/bust.ts
@@ -151,6 +151,32 @@ export async function bustOnTicketScanned(
     itemTags.ticket(ticketId),
     itemTags.ticketShort(shortId),
     adminTicketTags.scannedForEvent(eventId),
+    adminTicketTags.statsForEvent(eventId),
+    adminTicketTags.summaryForEvent(eventId),
+  ];
+
+  if (userId) {
+    tags.push(userTags.tickets(userId), userTags.ticketsUpcoming(userId));
+  }
+
+  await bustTags(tags);
+}
+
+/**
+ * Bust caches when ticket scan is reverted
+ */
+export async function bustOnTicketScanReverted(
+  ticketId: string,
+  shortId: string,
+  eventId: string,
+  userId: string | null
+) {
+  const tags: CacheTag[] = [
+    itemTags.ticket(ticketId),
+    itemTags.ticketShort(shortId),
+    adminTicketTags.scannedForEvent(eventId),
+    adminTicketTags.statsForEvent(eventId),
+    adminTicketTags.summaryForEvent(eventId),
   ];
 
   if (userId) {
